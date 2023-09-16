c4eabb9998bfc2072fd9a7438145c26534e9fe40
Aryan447 <singharyan4477@gmail.com>
2023-09-16 21:21:59 +0530

    animations v1.1

--- components/Intro.js
+++ components/Intro.js
@@ -18,6 +18,12 @@ export default function Intro() {
   const textY = useTransform(scrollYProgress, [0, 1], [0, -100]);
   const chasingTextOpacity = useTransform(scrollYProgress, [0.2, 1], [0, 1]);
 
+  const leftTextX = useTransform(scrollYProgress, [0, 0.5], [0, -100]); // Move left
+  const rightTextX = useTransform(scrollYProgress, [0, 0.5], [0, 100]); // Move right
+
+  const leftImageX = useTransform(scrollYProgress, [0, 0.5], [-100, 0]); // Move left
+  const rightImageX = useTransform(scrollYProgress, [0, 0.5], [100, 0]); // Move right
+
   const linkMotionProps = {
     whileHover: { scale: 1.1 },
     whileTap: { scale: 0.9 }
@@ -34,19 +40,32 @@ export default function Intro() {
 
   return (
     <>
+      <head>
+        <title>Aryan's Portfolio</title>
+      </head>
       <div className="mt-56 -ml-4 font-extrabold text-4xl">
         <motion.div className="font-extrabold text-4xl" variants={containerVariants} initial="hidden" animate="visible">
           <div className="parallax-container">
-            <motion.div className="parallax-content" style={{ opacity: textOpacity, y: textY }}>
+            <motion.div className="parallax-content" style={{ opacity: textOpacity }}>
               <div className="flex">
-                <h1 className="font-inter invisible lg:visible -mr-56">Hey There, I&apos;m</h1>
+                <motion.h1 className="large-screen font-inter invisible lg:visible -mr-60" style={{ x: leftTextX }}>
+                  Hey There, I'm
+                </motion.h1>
                 <div className="grid">
-                  <h1 className="font-inter lg:invisible -ml-1">Hey There</h1>
-                  <h1 className="font-inter lg:invisible -ml-1">I&apos;m</h1>
+                  <motion.h1 className="font-inter lg:invisible -ml-1" style={{ x: leftTextX }}>
+                    Hey There
+                  </motion.h1>
+                  <motion.h1 className="font-inter lg:invisible -ml-1" style={{ x: leftTextX }}>
+                    I'm
+                  </motion.h1>
                 </div>
-                <h1 className="gradient mt-10 -ml-28 lg:ml-20 lg:mt-0 gradient-text font-inter">ARYAN</h1>
+                <motion.h1 className="gradient mt-10 -ml-28 lg:ml-20 lg:mt-0 gradient-text font-inter" style={{ x: leftTextX }}>
+                  ARYAN
+                </motion.h1>
               </div>
-              <h1 className="font-inter lg:-mt-8 ml-0.5">I Turn Coffee into Code</h1>
+              <motion.h1 className="font-inter lg:-mt-8 ml-0.5" style={{ x: leftTextX }}>
+                I Turn Coffee into Code
+              </motion.h1>
             </motion.div>
           </div>
         </motion.div>
@@ -56,16 +75,35 @@ export default function Intro() {
       </div>
       <div className="lg:flex">
         <div className="mt-10 flex ml-3 gap-2 mr-3">
-          <Image src={img7} width={190} height={200} className="rounded-xl" />
-          <Image src={img2} width={190} height={200} className="rounded-xl" />
+          <motion.div style={{ x: leftImageX, transition: { delay: 0.2 } }}>
+            <Image src={img7} width={190} height={200} className="rounded-xl" />
+          </motion.div>
+          <motion.div style={{ x: rightImageX, transition: { delay: 0.2 } }}>
+            <Image src={img2} width={190} height={200} className="rounded-xl" />
+          </motion.div>
+
+          {/* <Image src={img7} width={190} height={200} className="rounded-xl" /> */}
+          {/* <Image src={img2} width={190} height={200} className="rounded-xl" /> */}
         </div>
         <div className="mt-10 flex ml-3 gap-2 mr-3">
-          <Image src={img3} width={190} height={200} className="rounded-xl" />
-          <Image src={img4} width={190} height={200} className="rounded-xl" />
+          <motion.div style={{ x: leftImageX, transition: { delay: 0.4 } }}>
+            <Image src={img3} width={190} height={200} className="rounded-xl" />
+          </motion.div>
+          <motion.div style={{ x: rightImageX, transition: { delay: 0.4 } }}>
+            <Image src={img4} width={190} height={200} className="rounded-xl" />
+          </motion.div>
+          {/* <Image src={img3} width={190} height={200} className="rounded-xl" /> */}
+          {/* <Image src={img4} width={190} height={200} className="rounded-xl" /> */}
         </div>
         <div className="mt-10 flex ml-3 gap-2 mr-3">
-          <Image src={img5} width={190} height={200} className="rounded-xl" />
-          <Image src={img6} width={190} height={200} className="rounded-xl" />
+          <motion.div style={{ x: leftImageX, transition: { delay: 0.6 }}}>
+            <Image src={img5} width={190} height={200} className="ml-2 rounded-xl" />
+          </motion.div>
+          <motion.div style={{ x: rightImageX, transition: { delay: 0.6 }}}>
+            <Image src={img6} width={190} height={200} className="ml-6 rounded-xl" />
+          </motion.div>
+          {/* <Image src={img5} width={190} height={200} className="rounded-xl" /> */}
+          {/* <Image src={img6} width={190} height={200} className="rounded-xl" /> */}
         </div>
       </div>
       <motion.button
